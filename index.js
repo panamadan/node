37927e24058555924be38333352dc8f60c6835a0
Juan De La Guardia <delaguardia@outlook.com>
2019-12-27 12:28:21 -0500

    prompt quesitons

--- index.js
+++ index.js
@@ -2,3 +2,30 @@ const inquirer = require("inquirer");
 const axios = require("axios");
 const electron = require("electron");
 const generateHtml = require('./generateHTML');
+
+inquirer.prompt([
+    {
+        type: "input",
+        message: "Enter your Git Hub Username:",
+        name: "username"
+
+    },
+    {
+        type: "list",
+        message: "What is your Favorite color..?",
+        choices: ["red", "blue", "green", "pink"],
+        name: "colors"
+
+
+    }])
+    .then(function (answers) {
+        console.log(answers);
+
+
+
+
+
+    
+
+
+    })
